107501b5d92f0f718b3139945114db47e5cfa299
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-09-16 08:55:09 +0300

    Added bootstrap and changed the manifest files.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,9 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery
+//= require jquery/dist/jquery
+//= require angular/angular
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
 //= require_self
 
